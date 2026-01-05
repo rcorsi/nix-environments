e3098ddb92fadea01fe9afcb7bcb968ccc2bd9af
Rocco Corsi <5201151+rcorsi@users.noreply.github.com>
2026-01-05 15:30:50 -0500

    ladybird: add vulkan validation layers

--- envs/ladybird/shell.nix
+++ envs/ladybird/shell.nix
@@ -20,6 +20,9 @@ pkgs.mkShell {
     ccache
     clang-tools
     nodePackages.prettier
+    vulkan-tools-lunarg
+    vulkan-tools
+    vulkan-validation-layers
   ] ++ extraPkgs;
 
   # https://github.com/NixOS/nixpkgs/blob/79a8a723b9/pkgs/by-name/la/ladybird/package.nix#L144-L147
